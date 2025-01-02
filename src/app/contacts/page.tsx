bed93abd3e3c16b9f9366994d018ac7cbd16c2f6
maksim-atanasov <atanasovmaksim56@gmail.com>
2025-01-02 18:54:14 +0200

    form validator added and changed meta data

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,12 +1,63 @@
-const page = () => {
+'use client';
+
+import { useState } from 'react';
+
+const emptyForm = {
+	name: '',
+	email: '',
+	message: '',
+};
+
+const Page = () => {
+	const [formValue, setFormValue] = useState(emptyForm);
+	const [isSended, setIsSended] = useState(false);
+	const [isError, setIsError] = useState(false);
+
+	const handleUpdateForm = (
+		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+		target: 'name' | 'email' | 'message',
+	) => {
+		setIsError(false);
+		setFormValue({ ...formValue, [target]: e.target.value });
+	};
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		if (
+			!formValue.name.trim() ||
+			!formValue.email.trim() ||
+			!formValue.message.trim()
+		) {
+			setIsError(true);
+			return;
+		}
+
+		setFormValue(emptyForm);
+		setIsSended(true);
+	};
+
 	return (
-		<div className='max-w-6xl mx-auto px-4 py-16'>
+		<main className='max-w-6xl mx-auto px-4 py-16'>
 			<h1 className='text-3xl font-bold mb-8 text-center'>Contact Us</h1>
 
-			<div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
-				<div className='bg-white rounded-lg shadow-md p-6'>
-					<h2 className='text-xl font-semibold mb-4'>Send us a message</h2>
-					<form>
+			<section className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+				<article className='bg-white rounded-lg shadow-md p-6'>
+					<h2 className='relative text-xl font-semibold mb-4'>
+						Send us a message
+						{isSended && (
+							<p className='absolute text-green-500 right-0 top-0'>
+								Sended successfully
+							</p>
+						)}
+						{isError && (
+							<p className='absolute text-red-500 right-0 top-0'>
+								Invalid Fields
+							</p>
+						)}
+					</h2>
+
+					<form onSubmit={handleSubmit}>
 						<div className='mb-4'>
 							<label className='block text-gray-700 mb-2' htmlFor='name'>
 								Name
@@ -14,6 +65,8 @@ const page = () => {
 							<input
 								type='text'
 								id='name'
+								value={formValue.name}
+								onChange={(e) => handleUpdateForm(e, 'name')}
 								className='w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600'
 							/>
 						</div>
@@ -23,6 +76,8 @@ const page = () => {
 							</label>
 							<input
 								type='email'
+								value={formValue.email}
+								onChange={(e) => handleUpdateForm(e, 'email')}
 								id='email'
 								className='w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600'
 							/>
@@ -33,6 +88,8 @@ const page = () => {
 							</label>
 							<textarea
 								id='message'
+								value={formValue.message}
+								onChange={(e) => handleUpdateForm(e, 'message')}
 								rows={4}
 								className='w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600'
 							></textarea>
@@ -44,9 +101,9 @@ const page = () => {
 							Send Message
 						</button>
 					</form>
-				</div>
+				</article>
 
-				<div className='bg-white rounded-lg shadow-md p-6'>
+				<article className='bg-white rounded-lg shadow-md p-6'>
 					<h2 className='text-xl font-semibold mb-4'>Contact Information</h2>
 					<div className='space-y-4'>
 						<div>
@@ -78,10 +135,10 @@ const page = () => {
 							</p>
 						</div>
 					</div>
-				</div>
-			</div>
-		</div>
+				</article>
+			</section>
+		</main>
 	);
 };
 
-export default page;
+export default Page;
